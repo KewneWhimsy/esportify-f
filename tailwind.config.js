10c9c4dae85ef4497473ab09bb0ff3a80c94617d
KewneWhimsy <josshanel@gmail.com>
2024-11-17 07:52:54 +0100

    page d'acceuil et événemnets validé, derniers tests HTMX

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,19 @@ export default {
     './src/**/*.tsx',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#1F191D",
+        text: "#FFFFEF",
+        heading: "#FF6F47",
+        link: "#FF4D4D",
+        linkHover: "#FF0000",
+      },
+      fontFamily: {
+        body: ["system-ui", "sans-serif", "Arial"],
+        heading: ["Tahoma", "Verdana", "sans-serif", "Arial"],
+      },
+    },
   },
   plugins: [],
 }
