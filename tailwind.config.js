cb6aa75ab9a1840983609b84207932f3ba85792f
KewneWhimsy <josshanel@gmail.com>
2024-11-19 15:04:31 +0100

    Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,17 @@ export default {
       },
     },
   },
+  safelist: [
+    'bg-[#26232A]', // Forcer la conservation de cette classe
+    'hover:scale-105', // Et toute autre classe dynamique
+    'mt-4', 'px-4', 'py-2', 'text-2xl', 'font-bold', 'mb-4', 
+    'font-heading', 'text-heading', 'truncate-2-lines', 'border', 
+    'p-6', 'rounded-lg', 'shadow-lg', 'h-full', 'w-full', 'text-sm', 
+    'text-gray-400', 'text-lg', 'leading-tight', 'mb-2', 
+    'justify-between', 'border-[#E5E7EB]', 'p-4', 
+    'w-64', 'shadow-md', 'transition-transform', 'scale-105', 
+    'cursor-pointer', 'flex-shrink-0', 'gap-0.5'
+  ],
   plugins: [],
 }
 
